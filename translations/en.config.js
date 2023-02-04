fcf6aedb68d1347164333d7cba6cbcdb59a92ef4
jabedzaman <jabedzaman2004@gmail.com>
2023-02-04 18:59:54 +0530

    add en-US language

---
+++ translations/en.config.js
@@ -0,0 +1,60 @@
+//  home screen
+
+const search_placeholder = "Enter track or playlist URL";
+const select_source = "Select Source";
+const get_tracks = "Get Tracks";
+const clear_tracks = "Clear Tracks";
+const download_all = "Download All";
+
+const spotify = "Spotify";
+const youtube = "Youtube";
+const warning = "Warning";
+const warning_message = "Please select a source";
+const warning_ok = "OK";
+
+//  settings screen
+
+const settings = "Settings";
+const language = "Language";
+const theme = "Theme";
+const about = "About";
+const share_with_friends = "Share with friends";
+const report_a_bug = "Report a bug";
+const feedback_and_suggestions = "Feedback and suggestions";
+const suport = "Suport";
+
+const support_message =
+  "If you like this app, please consider supporting the developer.";
+const patreon = "Patreon";
+const remind_me_later = "Remind me later";
+
+// about screen
+
+const about_message = "Made with ❤️ by";
+
+const en = {
+  search_placeholder,
+  select_source,
+  get_tracks,
+  clear_tracks,
+  download_all,
+  spotify,
+  youtube,
+  warning,
+  warning_message,
+  warning_ok,
+  settings,
+  language,
+  theme,
+  about,
+  share_with_friends,
+  report_a_bug,
+  feedback_and_suggestions,
+  suport,
+  support_message,
+  patreon,
+  remind_me_later,
+  about_message,
+};
+
+export default en;
