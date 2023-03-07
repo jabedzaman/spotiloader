79c7d2209e17ecff41b7eaeb469e4ce729ace2c9
Vlad-Andrei Medințu <80452570+Vladutzu27@users.noreply.github.com>
2023-03-07 21:59:32 +0200

    Create ro-RO.config.js

---
+++ translations/ro-RO.config.js
@@ -0,0 +1,60 @@
+//  home screen
+
+const search_placeholder = "Introdu URL-ul playlistului sau al piesei";
+const select_source = "Selectează sursa";
+const get_tracks = "Obține piese";
+const clear_tracks = "Șterge piesele";
+const download_all = "Descarcă tot";
+
+const spotify = "Spotify";
+const youtube = "Youtube";
+const warning = "Atenție";
+const warning_message = "Vă rugăm selectați o sursă";
+const warning_ok = "OK";
+
+//  settings screen
+
+const settings = "Setări";
+const language = "Limbă";
+const theme = "Temă";
+const about = "Despre";
+const share_with_friends = "Distribuie cu prietenii";
+const report_a_bug = "Raportează un bug";
+const feedback_and_suggestions = "Feedback sau sugestii";
+const suport = "Suport";
+
+const support_message =
+  "Dacă îți place această aplicație, consideră să susții creatorul acesteia.";
+const patreon = "Patreon";
+const remind_me_later = "Amintește-mi mai târziu";
+
+// about screen
+
+const about_message = "Făcut cu ❤️ de";
+
+const ro = {
+  search_placeholder,
+  select_source,
+  get_tracks,
+  clear_tracks,
+  download_all,
+  spotify,
+  youtube,
+  warning,
+  warning_message,
+  warning_ok,
+  settings,
+  language,
+  theme,
+  about,
+  share_with_friends,
+  report_a_bug,
+  feedback_and_suggestions,
+  suport,
+  support_message,
+  patreon,
+  remind_me_later,
+  about_message,
+};
+
+export default ro;
