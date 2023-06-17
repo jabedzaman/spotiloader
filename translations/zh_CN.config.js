de9677a6174cd6cbecc4b32a56e8272b9ea72207
TC999 <88823709+TC999@users.noreply.github.com>
2023-06-17 23:16:05 +0800

    Add files via upload

---
+++ translations/zh_CN.config.js
@@ -0,0 +1,60 @@
+//  home screen
+
+const search_placeholder = "填入音频流或播放列表URL链接";
+const select_source = "选择源";
+const get_tracks = "获取音频流";
+const clear_tracks = "清理音频流";
+const download_all = "下载全部";
+
+const spotify = "Spotify";
+const youtube = "Youtube";
+const warning = "警告";
+const warning_message = "请选择一个源";
+const warning_ok = "确定";
+
+//  settings screen
+
+const settings = "设置";
+const language = "需要";
+const theme = "主题";
+const about = "关于";
+const share_with_friends = "分享给朋友";
+const report_a_bug = "反馈BUG";
+const feedback_and_suggestions = "反馈和建议";
+const suport = "支持";
+
+const support_message =
+  "如果您喜欢这个应用程序，请考虑支持开发人员。";
+const patreon = "Patreon";
+const remind_me_later = "稍后回复";
+
+// about screen
+
+const about_message = "用❤️制作，开发者：";
+
+const en = {
+  search_placeholder,
+  select_source,
+  get_tracks,
+  clear_tracks,
+  download_all,
+  spotify,
+  youtube,
+  warning,
+  warning_message,
+  warning_ok,
+  settings,
+  language,
+  theme,
+  about,
+  share_with_friends,
+  report_a_bug,
+  feedback_and_suggestions,
+  suport,
+  support_message,
+  patreon,
+  remind_me_later,
+  about_message,
+};
+
+export default en;
