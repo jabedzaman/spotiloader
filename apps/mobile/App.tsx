140742273e81c5ce9305560bd64f1297d5809a30
jabedzaman <hi@jabed.dev>
2023-10-15 12:11:19 +0530

    perf: move from pnpm to yarn

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        lagdi mainu jiven ambraan di queen, meethiyan gallan kare, kudi namkeen
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
